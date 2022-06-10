1e5a2f89f2c977d973749f78c1c208a2a01e1b1d
vvprivalov <vv.privalov@yandex.ru>
2022-06-10 12:04:11 +0300

    1

---
+++ src/main/java/ru/geekbrains/myMarket/MyMarketApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.myMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMarketApplication.class, args);
+	}
+
+}
